b545102e4fd4dd2726f50c43ccff405577636fd2
GuangchenJ <guangchen98.jiang@gmail.com>
2025-11-16 22:01:31 +0800

    update-1116

--- shuffle_tex.py
+++ shuffle_tex.py
@@ -91,10 +91,10 @@ if __name__ == "__main__":
 
     # 1. 设置包含 .tex 错题文件的目标文件夹
     #    (使用正斜杠 / 作为路径分隔符)
-    TARGET_DIRECTORY = "./zhenti/gailv"
+    TARGET_DIRECTORY = "./zonghecuoti/1116"
 
     # 2. 设置输出的文件名
-    OUTPUT_FILENAME = "./zhenti/gailv/GaiLvZhenTi_shuffled_problems.tex"
+    OUTPUT_FILENAME = "./zonghecuoti/1116/all_shuffled_problems.tex"
 
     # -------------------------------------------------
 
